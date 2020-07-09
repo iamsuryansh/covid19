f99851885adbdf10f95acb25412183bc5d496596
Suryansh Jain <54480215+iamsuryansh@users.noreply.github.com>
2020-07-09 16:50:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pydeck as pdk
 import plotly.express as px
 from matplotlib import pyplot
 import datetime
@@ -27,7 +26,30 @@ if statewiseData:
     series2=series1.loc[[selectedState]]
     "Data for", selectedState
     series2
-
+    stateDict={"Andhra Pradesh":"AP","Arunachal Pradesh":"AR","Assam":"AS","Bihar":"BR","Chhattisgarh":"CG",
+                "Goa":"GA","Gujarat":"GJ","Haryana":"HR","Himachal Pradesh":"HP","Jammu and Kashmir":"JK","Jharkhand":"JH","Karnataka":"KA",
+                "Kerala":"KL","Madhya Pradesh":"MP","Maharashtra":"MH","Manipur":"MN","Meghalaya":"ML","Mizoram":"MZ",
+                "Nagaland":"NL","Orissa":"OR","Punjab":"PB","Rajasthan":"RJ","Sikkim":"SK","Tamil Nadu":"TN","Tripura":"TR",
+                "Uttarakhand":"UK","Uttar Pradesh":"UP","West Bengal":"WB","Tamil Nadu":"TN","Tripura":"TR","Andaman and Nicobar Islands":"AN",
+                "Chandigarh":"CH","Dadra and Nagar Haveli":"DH","Daman and Diu":"DD","Delhi":"DL","Lakshadweep":"LD","Pondicherry":"PY"}
+    series_statewise_daily = pd.read_csv(DATA_URL_statewise_timeseries, header=0, index_col=0)
+    stateForTimeSeries=stateDict[selectedState]
+    timeSeriesDataforLast30DaysConfirmed = series_statewise_daily[stateForTimeSeries][-90::3]
+    timeSeriesDataforLast30DaysRecovered = series_statewise_daily[stateForTimeSeries][-89::3]
+    timeSeriesDataforLast30DaysDeceased = series_statewise_daily[stateForTimeSeries][-88::3]
+    listOfVariables=["Daily Confirmed","Daily Recovered","Daily Deceased"]
+    option = st.selectbox(
+        'Select a category:',
+        listOfVariables)
+    if option=="Daily Confirmed":
+        figD=px.line(timeSeriesDataforLast30DaysConfirmed,title="Daily confirmed cases in {s}".format(s=selectedState),labels={stateForTimeSeries:selectedState})
+    elif option=="Daily Recovered":
+        figD=px.line(timeSeriesDataforLast30DaysRecovered,title="Daily recovered cases in {s}".format(s=selectedState))
+    elif option=="Daily Deceased":
+        figD=px.line(timeSeriesDataforLast30DaysDeceased,title="Daily deceased cases in {s}".format(s=selectedState))
+    figD.update_yaxes(title_text='No. of Cases')
+    figD.update_layout(legend_title_text = "State")
+    st.write(figD)
     st.subheader("Data for all States and Union Territories")
     fig = go.Figure(data=[go.Table(header=dict(values=["States","Confirmed","Recovered","Deaths","Active"]),
                  cells=dict(values=[series1.index,series1.Confirmed,series1.Recovered,series1.Deaths,series1.Active],
@@ -61,6 +83,7 @@ else:
     fig = px.line(linedata)
     fig.update_xaxes(title_text='Date')
     fig.update_layout(hovermode="x")
+    fig.update_layout(legend_title_text = "Parameter")
     fig.update_yaxes(title_text='No. of Cases')
     st.write(fig)
 
